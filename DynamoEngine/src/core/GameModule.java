f011a2cc896d710b0dce7803307560df6f7a4a10
Joe Muoio <jgm55@drexel.edu>
2013-02-14 09:36:46 -0500

    Moved the game loop to the game engine and just call it in the main demo

--- DynamoEngine/src/core/GameModule.java
+++ DynamoEngine/src/core/GameModule.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.awt.event.KeyEvent;
+import java.util.Date;
 
 import inputters.KeyboardInput;
 import inputters.MouseInput;
@@ -13,7 +14,11 @@ public class GameModule {
 	private static KeyboardInput keyboard;
 	private static MouseInput mouse;
 	private static RenderModule renderer;
-	
+	private static int REDRAWING_PERIOD = 20; 
+	private static int MAX_FRAME_SKIP = 10;
+	/**
+	 * Default constructor
+	 */
 	public GameModule(){
 		pathfinding = new PathModule();
 		objects = new ObjectModule();
@@ -21,6 +26,37 @@ public class GameModule {
 		keyboard=new KeyboardInput();
 		mouse = new MouseInput();
 		renderer = new RenderModule();
+		MAX_FRAME_SKIP=10;
+		REDRAWING_PERIOD=20;
+	}
+	/**
+	 * 
+	 * @param reDrawPeriod the period that it redraws in
+	 */
+	public GameModule(int reDrawPeriod){
+		pathfinding = new PathModule();
+		objects = new ObjectModule();
+		unitai = new AiModule();
+		keyboard=new KeyboardInput();
+		mouse = new MouseInput();
+		renderer = new RenderModule();
+		MAX_FRAME_SKIP=10;
+		REDRAWING_PERIOD=reDrawPeriod;
+	}
+	/**
+	 * 
+	 * @param reDrawPeriod the period that it redraws in
+	 * @param maxSkip the maximum frames skipped
+	 */
+	public GameModule(int reDrawPeriod, int maxSkip){
+		pathfinding = new PathModule();
+		objects = new ObjectModule();
+		unitai = new AiModule();
+		keyboard=new KeyboardInput();
+		mouse = new MouseInput();
+		renderer = new RenderModule();
+		MAX_FRAME_SKIP=maxSkip;
+		REDRAWING_PERIOD=reDrawPeriod;
 	}
 	/**
 	 * 
@@ -55,6 +91,54 @@ public class GameModule {
 	public boolean render(){
 		renderer.update(1);//will be our data structure
 		return true;
+	}
+	public void loop(){
+		long time = new Date().getTime(),cur_time; //ms
+		
+		int frames;
+		boolean need_to_redraw = true; 
+		boolean quit = false;
+		
+		
+		while (!quit) { 
+			
+			//System.pollForOSMessages();
+			
+			//updates mouse and keyboard input (by default)
+			quit=updateFromInput();
+		
+			cur_time = new Date().getTime(); 
+			frames = 0; 
+			while (cur_time - time >= REDRAWING_PERIOD && frames<MAX_FRAME_SKIP){
+				
+				time+=REDRAWING_PERIOD; 
+				/*
+				keyboard->cycle(); 
+				*/				
+				if (!cycle()){
+					quit = true;
+				}
+				cur_time = new Date().getTime(); 
+				need_to_redraw=true; 
+				frames++; 
+			}  
+			if (time < cur_time){
+				time = cur_time; 
+			}
+			if (need_to_redraw) { 
+				render();
+				need_to_redraw=false; 
+			} 
+			
+			//SDL_Delay(1); 
+			try {
+			    Thread.sleep(1);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}
+			
+			
+		}
 	}
 	
 }
